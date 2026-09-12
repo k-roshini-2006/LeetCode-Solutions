e6712c734313c211fdb103669e1919bc41b1a977
ROSHINI K <roshnikumar773@gmail.com>
2026-09-12 10:12:39 +0530

    Time: 108 ms (55.76%) | Memory: 47.3 MB (74.04%) - LeetSync

--- 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.java
+++ 3892-best-time-to-buy-and-sell-stock-v/best-time-to-buy-and-sell-stock-v.java
@@ -1,17 +1,17 @@
 class Solution {
     public long maximumProfit(int[] prices, int k) {
         int n=prices.length;
-        long[][] dp=new long[k+1][3];
+        long[][] dp=new long[k+3][3];
         for(int i=0;i<=k;i++){
             dp[i][1]=Long.MIN_VALUE/2;
             dp[i][2]=Long.MIN_VALUE/2;
         }
         for(int price:prices){
             long[][] next=new long[k+1][3];
-            for(int t=0;t<=k;t++){
-                next[t][0]=dp[t][0];
-                next[t][1]=dp[t][1];
-                next[t][2]=dp[t][2];
+            for(int i=0;i<=k;i++){
+                next[i][0]=dp[i][0];
+                next[i][1]=dp[i][1];
+                next[i][2]=dp[i][2];
             }
             for(int i=0;i<k;i++){
                 next[i][1]=Math.max(next[i][1],dp[i][0]-price);
