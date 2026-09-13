8ea18b83dbae8bdcf0750d2ad1bb2821b576aa8f
ROSHINI K <roshnikumar773@gmail.com>
2026-09-13 16:37:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (87.52%) - LeetSync

--- 858-masking-personal-information/masking-personal-information.cpp
+++ 858-masking-personal-information/masking-personal-information.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     string maskPII(string s) {
-        if(s.find('@')!=string::npos){
-            string ans="";
-            for(char &ch:s){
+        if(s.find('@')!=-1){
+            for(char& ch:s){
                 ch=tolower(ch);
             }
-            ans=s[0];
+            string ans="";
+            ans+=s[0];
             ans+="*****";
             int at=s.find('@');
-            ans+=s[at-1];
-            ans+=s.substr(at);
+            ans+=s.substr(at-1);
             return ans;
         }
         string digit="";
@@ -20,19 +19,19 @@ public:
             }
         }
         int n=digit.length();
+        string last4=digit.substr(n-4);
         string ans="";
         ans+="***-***-";
-        string last4=digit.substr(n-4);
         ans+=last4;
-        if(n>10){
-            int country=n-10;
-            string prefix="+";
-            for(int i=0;i<country;i++){
-                prefix+="*";
-            }
-            prefix+="-";
-            ans=prefix+ans;
+       if(n>10){
+        int country=n-10;
+        string prefix="+";
+        for(int i=0;i<country;i++){
+            prefix+="*";
         }
-        return ans;
+        prefix+="-";
+        ans=prefix+ans;
+       }
+       return ans;
     }
 };
