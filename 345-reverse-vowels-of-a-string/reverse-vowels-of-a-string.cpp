b595205a019d9eee72213b3571b0d011243d9b50
ROSHINI K <roshnikumar773@gmail.com>
2026-09-14 09:04:38 +0530

    Time: 3 ms (32.43%) | Memory: 10.3 MB (73.32%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string reverseVowels(string s) {
         int left=0;
-        int right=s.length();
+        int right=s.length()-1;
         while(left<right){
             while(left<right && !isVowel(s[left])){
                 left++;
