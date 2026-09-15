1a93dace4d331b2f6cbe520b8abf7e3d95f68ebd
ROSHINI K <roshnikumar773@gmail.com>
2026-09-15 19:05:40 +0530

    Time: 2 ms (4.79%) | Memory: 47.5 MB (5.17%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -1,28 +1,26 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int n=nums.length;
-        List<Integer> evenlist=new ArrayList<>();
-        List<Integer> oddlist=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            if(nums[i]%2==0){
-                evenlist.add(nums[i]);
+        List<Integer> even=new ArrayList<>();
+        List<Integer> odd=new ArrayList<>();
+        for(int i:nums){
+            if(i%2==0){
+                even.add(i);
             }
             else{
-                oddlist.add(nums[i]);
+                odd.add(i);
             }
         }
         List<Integer> list=new ArrayList<>();
-        for(int i=0;i<evenlist.size();i++){
-            list.add(evenlist.get(i));
+        for(int i:even){
+            list.add(i);
         }
-         for(int i=0;i<oddlist.size();i++){
-            list.add(oddlist.get(i));
+        for(int i:odd){
+            list.add(i);
         }
-        int[] result=new int[n];
-        for(int i=0;i<n;i++){
-            result[i]=list.get(i);
+        int[] ans=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            ans[i]=list.get(i);
         }
-
-        return result;
+        return ans;
     }
 }
