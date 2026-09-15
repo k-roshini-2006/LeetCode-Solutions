1fa33325b99ba9d89fcca34ce8b75a6cc9ab0d04
ROSHINI K <roshnikumar773@gmail.com>
2026-09-15 15:53:26 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (14.40%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/remove-duplicate-letters.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
+        stack<char> st;
+        string sb;
         vector<int> lastIndex(26);
-        stack<int> st;
-        vector<bool> visited(26);
         for(int i=0;i<s.length();i++){
             lastIndex[s[i]-'a']=i;
         }
+        vector<bool> visited(26);
         for(int i=0;i<s.length();i++){
             char ch=s[i];
             if(visited[ch-'a']){
                 continue;
             }
             while(!st.empty() && st.top()>ch && lastIndex[st.top()-'a']>i){
-               visited[st.top()-'a']=false;
-               st.pop();
+                visited[st.top()-'a']=false;
+                st.pop();
             }
             st.push(ch);
             visited[ch-'a']=true;
         }
-        string sb;
+        string ans;
         while(!st.empty()){
-            sb+=st.top();
+            ans+=st.top();
             st.pop();
         }
-        reverse(sb.begin(),sb.end());
-        return sb;
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
