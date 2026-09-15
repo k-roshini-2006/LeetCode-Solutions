1ce81b14f2a1631d69a8d3f63d41d41a3bf38b9f
ROSHINI K <roshnikumar773@gmail.com>
2026-09-15 15:04:05 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (45.02%) - LeetSync

--- 1933-number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.cpp
+++ 1933-number-of-different-integers-in-a-string/number-of-different-integers-in-a-string.cpp
@@ -3,11 +3,11 @@ public:
     int numDifferentIntegers(string word) {
         unordered_set<string> set;
         string sb;
-        int n=word.length();
-        for(int i=0;i<=n;i++){
-            if(isdigit(word[i])){
+        for(int i=0;i<=word.length();i++){
+            if(i<word.length() && isdigit(word[i])){
                 sb+=word[i];
-            }else{
+            }
+            else{
                 if(sb.length()>0){
                     while(sb.length()>1 && sb[0]=='0'){
                         sb.erase(0,1);
