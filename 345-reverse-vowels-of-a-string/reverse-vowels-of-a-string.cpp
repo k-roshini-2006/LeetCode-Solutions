4ce2196d0402dfef85685457b432f9fcc198c70c
ROSHINI K <roshnikumar773@gmail.com>
2026-09-17 21:19:48 +0530

    Time: 4 ms (18.83%) | Memory: 11.2 MB (8.83%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     string reverseVowels(string s) {
-        vector<char> vowel;
-        for(char ch:s){
-            if(isVowel(ch)){
-                vowel.push_back(ch);
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (!isVowel(s[left])) {
+                left++;
+            }
+            else if (!isVowel(s[right])) {
+                right--;
+            }
+            else {
+                char temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                left++;
+                right--;
             }
         }
-        reverse(vowel.begin(),vowel.end());
-        int j=0;
-        for(int i=0;i<s.length();i++){
-            if(isVowel(s[i])){
-                s[i]=vowel[j];
-                j++;
-            }
-        }
+
         return s;
     }
-    bool isVowel(char ch){
-        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U';
+
+    bool isVowel(char ch) {
+        return string("aeiouAEIOU").find(ch) != string::npos;
     }
 };
