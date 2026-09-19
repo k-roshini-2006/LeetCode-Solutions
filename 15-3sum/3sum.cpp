927f0897872ce946fa9677b60bcc41088fde4800
ROSHINI K <roshnikumar773@gmail.com>
2026-09-19 09:26:08 +0530

    Time: 53 ms (30.75%) | Memory: 29.1 MB (45.90%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -21,13 +21,14 @@ public:
                     list.push_back({nums[i],nums[left],nums[right]});
                     left++;
                     right--;
-                while(left<right && nums[left]==nums[left-1]){
-                    left++;
+                    while(left<right && nums[left]==nums[left-1]){
+                        left++;
+                    }
+                    while(left<right && nums[right]==nums[right+1]){
+                        right--;
+                    }
                 }
-                while(left<right && nums[right]==nums[right+1]){
-                    right--;
-                }
-                }
+
             }
         }
         return list;
