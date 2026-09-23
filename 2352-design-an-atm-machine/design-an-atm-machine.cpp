929421fc7468cb9d5b00186e42b649e83b20b8e9
ROSHINI K <roshnikumar773@gmail.com>
2026-09-23 09:50:47 +0530

    Time: 18 ms (34.68%) | Memory: 109.5 MB (45.06%) - LeetSync

---
+++ 2352-design-an-atm-machine/design-an-atm-machine.cpp
@@ -0,0 +1,37 @@
+class ATM {
+public:
+    vector<long long> notes;
+    vector<int> denomination;
+    ATM() {
+        notes=vector<long long>(5,0);
+        denomination={20,50,100,200,500};
+    }
+    void deposit(vector<int> banknotesCount) {
+        for(int i=0;i<5;i++){
+            notes[i]+=banknotesCount[i];
+        }
+    }
+    vector<int> withdraw(int amount) {
+        vector<int> used(5,0);
+            int remain=amount;
+            for(int i=4;i>=0;i--){
+                long long canTake=min(notes[i],(long long)remain/denomination[i]);
+                used[i]=canTake;
+                remain-=canTake*denomination[i];
+            }
+            if(remain!=0){
+                return {-1};
+            }
+            for(int i=0;i<5;i++){
+                notes[i]-=used[i];
+            }
+            return used;
+    }
+};
+
+/**
+ * Your ATM object will be instantiated and called as such:
+ * ATM* obj = new ATM();
+ * obj->deposit(banknotesCount);
+ * vector<int> param_2 = obj->withdraw(amount);
+ */
